5427b52afe2969eea9ccffcef09eb473a0e721a3
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-16 11:47:34 +0900

    [Bronze III] Title: 최댓값, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0;
+        int b = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            int c = sc.nextInt();
+            if (c > a) {
+                a = c;
+                b = i;
+            }
+        }
+        System.out.println(a);
+        System.out.println(b);
+
+        sc.close();
+    }
+}
